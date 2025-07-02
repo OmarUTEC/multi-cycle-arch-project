9456e1f851acd8c8d721c804cde7ebd8b243618b
NoeParedes <ananias.paredes@utec.edu.pe>
2025-07-02 18:54:28 -0500

    UMUL and SMUL

--- alu.v
+++ alu.v
@@ -2,6 +2,7 @@ module alu(
     input  [31:0] a, b,
     input  [2:0] ALUControl,
     output reg [31:0] Result,
+    output reg [31:0] ResultHi,
     output wire [3:0] ALUFlags
 );
 
@@ -10,10 +11,12 @@ module alu(
     wire [32:0] sum;
     wire is_logic;
 
-    wire signo_a, signo_b;
-    wire resultado_signo;
+    //para 64 bits
+    wire signo_a, signo_b, resultado_signo;
+    wire [31:0] abs_a, abs_b;
     wire [63:0] mul_unsigned;
-    wire [31:0] abs_a,abs_b,smul;
+    wire [63:0] smul_result;
+    wire [63:0] umul_result = a * b;
 
     assign condinvb = ALUControl[0] ? ~b : b;
     assign sum = a + condinvb + ALUControl[0];
@@ -30,16 +33,26 @@ module alu(
     //multiplicacion sin signo de los valores absolutos
     assign mul_unsigned = abs_a * abs_b;
     
-    // Aplicar signo al resultado
-    assign smul = resultado_signo ? (~mul_unsigned[31:0] + 1) : mul_unsigned[31:0];
+    // le damos signo al resultado
+    assign smul_result = resultado_signo ? (~mul_unsigned[63:0] + 1) : mul_unsigned[63:0];
     always @(*) begin
+        //ResultHi = 32'b0;
         case (ALUControl[2:0])
             3'b000, 3'b001: Result = sum;
             3'b010:       Result = a & b;
             3'b011:       Result = a | b;
-            3'b100:       Result = a ^ b;
             3'b111:       Result = a * b; // MUL
-            3'b110:       Result = smul;  //SMUL
+            3'b110:       begin //SMUL
+                Result = smul_result[31:0];
+                ResultHi = smul_result[63:32];
+            end
+            3'b101:       begin // UMUL
+                Result = umul_result [31:0];
+                ResultHi = umul_result [63:32];
+            end
+            
+            
+    
             default:     Result = 32'b0;
         endcase
     end
